c580eeac7360fb22515aca64e37a6c2056c915e1
rendertem <rendertem@gmail.com>
2017-03-06 21:50:46 +0100

    Update main.cpp
    English was now an supportet language. Add option --help and --no-lang.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
-#include <ctime>
-#include <cmath>
+#include <time.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 char version[8] = "1.0.0.0";
 
-int boolInInt(bool rv)
-{
-	if(rv)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
+char parameterHelp[7] = "--help";
+char parameterNoLang[10] = "--no-lang";
+
+int lang = 1; // 0=German 1=English
 
 struct byte
 {
@@ -32,6 +24,68 @@ struct byte
 	bool b7 : 1;
 };
 
+int strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	for(; *(s1 + i) != '\0'; i++)
+	{
+		if(*(s1 + i) != *(s2 + i))
+		{
+			return 1;
+		}
+	}
+	if(*(s1 + i) != *(s2 + i))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+void setLang()
+{
+	printf("1) Sollte ihre Sprache Deutsch sein tippen Sie 1.\n");
+	printf("2) Is your speak english type 2.\n");
+	int c = getchar();
+	getchar();
+	if(c == '1')
+	{
+		lang = 0;
+	}
+	else if(c == '2')
+	{
+		lang = 1;
+	}
+	else
+	{
+		printf("\"%c\" ist keine Verfügbare Sprache.\n", c);
+		printf("\"%c\" is an not avaibal speak.\n", c);
+	}
+}
+
+void help()
+{
+	if(lang == 0)
+	{
+		printf("Eingabedateien:\n\tmes.txt\n\tkey.txt\nAusgabedatei:\n\txor.txt\n");
+	}
+	else
+	{
+		printf("Inputfiles:\n\tmes.txt\n\tkey.txt\nOutputfiles:\n\txor.txt\n");
+	}
+}
+
+int boolInInt(bool rv)
+{
+	if(rv)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 int xorVerschlueseln()
 {
 	FILE *mesF;
@@ -45,17 +99,38 @@ int xorVerschlueseln()
 
 	if(mesF == NULL)
 	{
-		fprintf(stderr, "Fehler: \"mes.txt\" ist nicht vorhanden.\n");
+		if(lang == 0)
+		{
+			fprintf(stderr, "Fehler: \"mes.txt\" ist nicht vorhanden.\n");
+		}
+		else
+		{
+			fprintf(stderr, "Error: \"mes.txt\" not exist.\n");
+		}
 		dateiFehler = true;
 	}
 	if(keyF == NULL)
 	{
-		fprintf(stderr, "Fehler: \"key.txt\" ist nicht vorhanden.\n");
+		if(lang == 0)
+		{
+			fprintf(stderr, "Fehler: \"key.txt\" ist nicht vorhanden.\n");
+		}
+		else
+		{
+			fprintf(stderr, "Error: \"key.txt\" not exist.\n");
+		}
 		dateiFehler = true;
 	}
 	if(xorF == NULL)
 	{
-		fprintf(stderr, "Fehler: \"xor.txt\" ist nicht vorhanden.\n");
+		if(lang == 0)
+		{
+			fprintf(stderr, "Fehler: \"xor.txt\" ist nicht vorhanden.\n");
+		}
+		else
+		{
+			fprintf(stderr, "Error: \"xor.txt\" not exist.\n");
+		}
 		dateiFehler = true;
 	}
 	if(dateiFehler)
@@ -74,7 +149,14 @@ int xorVerschlueseln()
 			byte bits3;
 			if((c2 = getc(keyF)) == EOF)
 			{
-				fprintf(stderr, "Fehler: \"key.txt\" ist kürzer als \"mes.txt\".\n");
+				if(lang == 0)
+				{
+					fprintf(stderr, "Fehler: \"key.txt\" ist kürzer als \"mes.txt\".\n");
+				}
+				else
+				{
+					fprintf(stderr, "Error: \"key.txt\" is shorter as \"mes.txt\".\n");
+				}
 				return 1;
 			}
 			bits.b0 = (c & 0x01) != 0;
@@ -125,9 +207,38 @@ int xorVerschlueseln()
 	fclose(xorF);
 }
 
-int main()
+void printVersion()
 {
-	printf("Verschlüsellung mit xor.\nProgrammversion \"v%s\".\n\n", version);
-	printf("Eingabedateien:\n\tmes.txt\n\tkey.txt\nAusgabedatei:\n\txor.txt\n");
+	if(lang == 0)
+	{
+		printf("Verschlüsellung mit xor.\nProgrammversion \"v%s\".\n\n", version);
+	}
+	else
+	{
+		printf("Encoding with xor.\nProgram version \"v%s\".\n\n", version);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	char langSet = 0;
+	for(int i = 2; argc >= i; i++)
+	{
+		if(strcmp(argv[i - 1], parameterHelp) == 0)
+		{
+			printVersion();
+			help();
+			return 0;
+		}
+		if(strcmp(argv[i - 1], parameterNoLang) == 0)
+		{
+			langSet = 1;
+		}
+	}
+	if(langSet == 0)
+	{
+		setLang();
+	}
+	printVersion();
 	return xorVerschlueseln();
 }
